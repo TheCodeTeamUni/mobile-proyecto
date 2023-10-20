1997b2372dd069c93370b8608dc3c124e5a71276
Manuel Morales <mf.moraless1@uniandes.edu.co>
2023-10-20 12:22:07 -0500

    complite register user

--- app/src/main/java/com/example/vinilos/data/RegisterBody.kt
+++ app/src/main/java/com/example/vinilos/data/RegisterBody.kt
@@ -3,6 +3,6 @@ package com.example.vinilos.data
 data class RegisterBody(
     val username: String,
     val email: String,
-    val password: String
-    //val type: String
+    val password: String,
+    val type: String
 )
