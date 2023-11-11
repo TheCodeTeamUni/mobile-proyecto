4f349f8569f364a56a332a341764b9f0ad58466e
Manuel Morales <mf.moraless1@uniandes.edu.co>
2023-11-10 19:10:19 -0500

    add project view

--- app/src/main/java/com/example/vinilos/ui/main/view/CompanyPortalActivity.kt
+++ app/src/main/java/com/example/vinilos/ui/main/view/CompanyPortalActivity.kt
@@ -1,12 +1,24 @@
 package com.example.vinilos.ui.main.view
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.vinylsMobile.vinylsapplication.R
+import com.vinylsMobile.vinylsapplication.databinding.ActivityCompanyPortalBinding
+import com.vinylsMobile.vinylsapplication.databinding.ActivityProjectModuleBinding
+import com.vinylsMobile.vinylsapplication.databinding.ActivitySelectProfileBinding
 
 class CompanyPortalActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityCompanyPortalBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_company_portal)
+        binding = ActivityCompanyPortalBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.projectModuleIcon.setOnClickListener {
+            val intent = Intent(this, ProjectModuleActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/vinilos/ui/main/view/ProjectModuleActivity.kt
@@ -0,0 +1,12 @@
+package com.example.vinilos.ui.main.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.vinylsMobile.vinylsapplication.R
+
+class ProjectModuleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_project_module)
+    }
+}
